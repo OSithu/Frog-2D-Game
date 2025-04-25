1a43465b33a67c39137db6736abd927140a0563d
Osh Disanayake <107048011+OSithu@users.noreply.github.com>
2025-04-25 18:47:07 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1745586934,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/frog-sheet0.png",
+		"images/lilypad-sheet0.png",
+		"images/pond-sheet0.png",
+		"images/spritefont.png",
+		"images/rock-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png"
+	]
+}
